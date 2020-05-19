3f7b65109a3e08c3690d4eb08d26a9eb6f11a975
Ivy Tiongco <ivydoreen@gmail.com>
2020-05-19 11:49:00 -0700

    update app

--- static/js/app.js
+++ static/js/app.js
@@ -21,36 +21,74 @@ function unpack(rows, index) {
 //    }
 
     var sampleValuesArray = data.samples.map(object => object.sample_values); 
-    for (var i = 0; i < 10; i++) {  
-      var sampleValuesArraySliced = sampleValuesArray.slice(i, 10);
-//    var sampleValues = data.samples.sample_values.slice(0, 10);
-//    var otuIds = data.samples.otu_ids.slice(0, 10);
-//    var otuLabels = data.samples.otu_labels.slice(0, 10);
-    console.log(sampleValuesArray);
-    console.log(sampleValuesArraySliced);
-    }
-  
-  var trace1 = {
-    x: otuIds,
-    y: sampleValues,
-    type: "bar",    
-    orientation: "h"
-  };
+    var otuIds = data.samples.map(object => object.otu_ids); 
+    var otuLabels = data.samples.map(object => object.otu_labels);     
+//    console.log("sampleValuesArray: ", sampleValuesArray);
+//    console.log("otuIds: ", otuIds);
+//    console.log("otuLabels: ", otuLabels);
 
-  // Create the data array for the plot
-  var data = [trace1];
+//    var sampleValues940 = sampleValuesArray[0]
+//    console.log("sampleValues940: ", sampleValues940);
 
-  // Define the plot layout
-  var layout = {
-    title: "Top 10 OTUs",
-//    xaxis: { title: "Organ" },
-//    yaxis: { title: "Square Root of Survival" }
-  };
+//    var sampleValues940Sorted = sampleValuesArray[0].sort((a, b) => b.sampleValuesArray[0] - a.sampleValuesArray[0]);
+//    var otuIds940Sorted = otuIds[0].slice(0, 10);
+//    var otuLabels940Sorted = otuLabels[0].slice(0, 10);
+//    console.log("sampleValues940Sorted: ", sampleValues940Sorted);
+//    console.log("otuIds940Sorted: ", otuIds940Sorted);
+//    console.log("otuLabels940Sorted: ", otuLabels940Sorted);
 
-  // Plot the chart to a div tag with id "plot"
-  Plotly.newPlot("bar", data, layout);  
+    var sampleValues940Sliced = sampleValuesArray[0].slice(0, 10);
+    var otuIds940Sliced = otuIds[0].slice(0, 10);
+    var otuLabels940Sliced = otuLabels[0].slice(0, 10);
+    console.log("sampleValues940Sliced: ", sampleValues940Sliced);
+    console.log("otuIds940Sliced: ", otuIds940Sliced);
+    console.log("otuLabels940Sliced: ", otuLabels940Sliced);
+    
+    var otuIds940SlicedString = otuIds940Sliced.map(String);
+    console.log("otuIds940SlicedString: ", otuIds940SlicedString)
 
-  });
+    var otuIds940SlicedLabels = otuIds940SlicedString.forEach(function(element, index) {
+      otuIds940SlicedString[index] = 'OTU ' + element;
+    });
+    console.log("otuIds940SlicedLabels: ", otuIds940SlicedLabels);
+
+    // Sort the array in descending order    
+  //  var sampleValues940SlicedSorted = sampleValues940Sliced.sort(function compareFunction(firstNum, secondNum) {
+      // resulting order is (3, 2, 1)
+  //    return secondNum - firstNum;
+  //  });
+
+  //  console.log("sampleValues940SlicedSorted: ", sampleValues940SlicedSorted);
+
+    // Reverse the array to accommodate Plotly's defaults
+    reversedSampleValues940Sliced = sampleValues940Sliced.reverse();
+    reversedOtuIds940SlicedLabels = otuIds940SlicedLabels.reverse();
+
+
+    var trace1 = {
+      x: reversedSampleValues940Sliced,
+      y: otuIds940SlicedLabels,
+  //    text: otuLabels940Sliced,
+      name: "OTUs",
+      type: "bar",    
+      orientation: "h"      
+    };
+
+    // Create the data array for the plot
+    var data = [trace1];
+
+    // Define the plot layout
+    var layout = {
+      title: "Top 10 OTUs",
+  //    yaxis=dict(autorange="reversed")
+  //    xaxis: { title: "Organ" },
+  //    yaxis: { title: "Square Root of Survival" }
+    };
+
+    // Plot the chart to a div tag with id "plot"
+    Plotly.newPlot("bar", data, layout);  
+
+    });
 //}
 
 //buildPlot();
